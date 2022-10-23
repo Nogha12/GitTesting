9dd70effb9d9758877ae70c6209c39483e3585c1
Noah Lomu <nlomu@asu.edu>
2022-10-22 20:04:46 -0700

    minor changes

--- GitTesting/main.cpp
+++ GitTesting/main.cpp
@@ -10,7 +10,6 @@ int main()
     testPrint(TEST_MESSAGE);
 
     std::cout << "42 plus 69 is " << testAdd(42, 69) << std::endl;
-
     std::cout << "69 divided by 3 is " << testDivide(69, 3, &remainder) << std::endl;
     std::cout << "\tthe remainder is " << remainder << std::endl;
 
